4c48777e054c05b07b4cafee6d32f20fc3eb6b11
Merabi Khatiashvili <mkhatiashvili@dga.gov.ge>
2023-08-18 12:01:56 +0400

    remove autoplay

--- SignallingWebServer/Public/player.js
+++ SignallingWebServer/Public/player.js
@@ -4096,7 +4096,7 @@
               (this.videoElement.style.height = '100%'),
               (this.videoElement.style.position = 'absolute'),
               (this.videoElement.style.pointerEvents = 'all'),
-              (this.videoElement.autoplay = !0),
+              // (this.videoElement.autoplay = !0),
               e.appendChild(this.videoElement),
               (this.onResizePlayerCallback = () => {
                 console.log(
